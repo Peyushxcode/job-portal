a61d9086daba6fb736a7de99aa91b72fb2fba5be
Mudit Kumar Singh <muditksingh96184@gmail.com>
2026-02-24 09:39:00 +0530

    Mongoose connection

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
-const express = require("express")
+require("dotenv").config()
+
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const express = require("express");
+const connectDB = require("./config/dbConnect");
 const app = express();
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 3500;
+
+connectDB()
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+
+
+// app.use('/jobs',require("./routes/jobsRoutes"))
+
+
+mongoose.connection.once('open', () => {
+    console.log("Connected to mongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+})
 
 
