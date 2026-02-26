b698acc59df113a7e184a5781746bf16b8ac0b0c
Mudit Kumar Singh <muditksingh96184@gmail.com>
2026-02-26 09:58:45 +0530

    26-02-2026

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,42 +2,50 @@ const User = require("../models/Users")
 
 
 const getAllUsers = async (req, res) => {
-    const users = await User.find().select('-password').lean();
+    try {
+        const users = await User.find().select('-password').lean();
 
-    if (!users) {
-        return res.status(400).json({ message: "No users found" })
+        if (!users) {
+            return res.status(400).json({ message: "No users found" })
+        }
+
+        res.json(users)
+    } catch (err) {
+        console.log(err);
     }
-
-    res.json(users)
 }
 
 const addUser = async (req, res) => {
-    const { username, email, password, location, gender, role, resume } = req.body;
+    try {
+        const { username, email, password, location, gender, role, resume } = req.body;
 
-    if (!username || !email || !password || !gender || !role || !resume) {
-        return res.status(400).json({ message: "All field are required" })
-    }
+        if (!username || !email || !password || !gender || !role || !resume) {
+            return res.status(400).json({ message: "All field are required" })
+        }
 
-    const duplicate = await User.findOne({ email }).collation({ locale: 'en', strength: 2 }).lean().exec()
+        const duplicate = await User.findOne({ email }).collation({ locale: 'en', strength: 2 }).lean().exec()
 
-    if (duplicate) {
-        return res.status(409).json({ message: "User already exist" })
-    }
+        if (duplicate) {
+            return res.status(409).json({ message: "User already exist" })
+        }
 
-    const user = await User.create({
-        username,
-        email,
-        password,
-        location,
-        gender,
-        role,
-        resume
-    })
+        const user = await User.create({
+            username,
+            email,
+            password,
+            location,
+            gender,
+            role,
+            resume
+        })
 
-    if (user) {
-        res.status(201).json({ message: `New user ${username} created` })
-    } else {
-        res.status(400).json({ message: "Invalid data recevied" })
+        if (user) {
+            res.status(201).json({ message: `New user ${username} created` })
+        } else {
+            res.status(400).json({ message: "Invalid data recevied" })
+        }
+    } catch (err) {
+        console.log(err);
     }
 
 
